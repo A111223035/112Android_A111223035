38a9c8912912e275b2401d7ee19e7373226424ec
A111223035 <a111223035@mail.shu.edu.tw>
2024-06-06 17:47:38 +0800

    0606

--- 0523/ExplicitintentDemo2/app/src/main/java/com/example/explicitintentdemo2/MainActivity.java
+++ 0523/ExplicitintentDemo2/app/src/main/java/com/example/explicitintentdemo2/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.explicitintentdemo2;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -7,16 +9,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 public class MainActivity extends AppCompatActivity {
-    private TextView output;
 
+    private TextView output;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,10 +27,10 @@ public class MainActivity extends AppCompatActivity {
                 txtOpd2 = (EditText) findViewById(R.id.txtOpd2);
                 Intent intent = new Intent(MainActivity.this, OpActivity.class);
                 Bundle bundle = new Bundle();
-                bundle.putString("OPERAND1", txtOpd1.getText().toString());
-                bundle.putString("OPERAND2", txtOpd2.getText().toString());
+                bundle.putString("OPERAND01",txtOpd1.getText().toString());
+                bundle.putString("OPERAND02",txtOpd2.getText().toString());
                 intent.putExtras(bundle);
-                startActivityForResult(intent,1);
+                startActivityForResult(intent, 1);
             }
         });
     }
